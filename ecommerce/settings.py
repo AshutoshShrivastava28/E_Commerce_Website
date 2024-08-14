8701b0f97870da6b8adcc0f9abc0f54b09f472b4
Ashutosh Shrivastava <ashu28.shrivastava@gmail.com>
2024-08-14 10:58:34 +0530

    Updated requirements

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "#vw(03o=(9kbvg!&2d5i!2$_58x@_-3l4wujpow6(ym37jxnza"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -124,10 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+# STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static ")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static")
 
